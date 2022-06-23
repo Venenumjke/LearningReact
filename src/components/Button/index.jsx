a2e190209a4c98d59b5971ec287e3f318f986d4b
Denya Mochalov <denbot3@mail.ru>
2022-06-23 17:08:04 +0300

    vpizduetiknopki

--- src/components/Button/index.jsx
+++ src/components/Button/index.jsx
@@ -2,14 +2,14 @@ import React, { memo, useCallback } from 'react';
 import styles from './style';
 
 // eslint-disable-next-line react/prop-types
-const Button = memo(({ text = '', onPress = () => console.log('Please attach method') }) => {
+const Button = memo(({ text = '', onPress = () => console.log('Please attach method'), disabled = false }) => {
   const onPressHandle = useCallback(() => {
     onPress();
   }, [onPress]);
 
   return (
   // eslint-disable-next-line react/button-has-type
-    <button style={styles.buttonWrapper} onClick={onPressHandle}>
+    <button disabled={disabled} style={styles.buttonWrapper} onClick={onPressHandle}>
       <p style={styles.buttonText}>{text}</p>
     </button>
   );
