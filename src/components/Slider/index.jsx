8e386d232929914e5c33a98241046fcc7683179c
Denya Mochalov <denbot3@mail.ru>
2022-06-24 10:32:34 +0300

    yaslomal

---
+++ src/components/Slider/index.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Button from '../Button';
+import styles from './styles';
+
+function Slider() {
+  return (
+    <main style={styles.main}>
+      <Button />
+      <Button />
+    </main>
+  );
+}
+
+export default Slider;
