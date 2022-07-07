c0a285d08ec8921b1a848570238a8fa120623928
Denya Mochalov <denbot3@mail.ru>
2022-07-07 17:01:56 +0300

    brokenVersionqq

---
+++ src/components/Slider/TranslateTest/index.jsx
@@ -0,0 +1,125 @@
+import React, {
+  useState, useCallback, useEffect, memo, useRef,
+} from 'react';
+
+import Button from '../Button';
+
+import style from './styles';
+import TextInput from '../TextInput';
+
+const MOCK_DATA = {
+  tree: 'дерево',
+  grass: 'трава',
+  mother: 'мама',
+  father: 'папа',
+  brother: 'брат',
+  syringe: 'шприц',
+  medicine: 'лекарство',
+  glass: 'стекло',
+  train: 'поезд',
+  stomach: 'желудок',
+  teeth: 'зубы',
+  hair: 'волосы',
+  leg: 'нога',
+};
+
+const keys = Object.keys(MOCK_DATA);
+const values = Object.values(MOCK_DATA);
+
+const TranslateTest = memo(() => {
+  const [isStart, setIsStart] = useState(false);
+  const [aroundScore, setAroundScore] = useState(0);
+  const [status, setStatus] = useState('Нажмите старт для начала теста');
+  const [rightTranslate, setRightTranslate] = useState();
+  const [inputValue, setInputValue] = useState('');
+  const [index, setIndex] = useState(0);
+  const [fail, setFail] = useState(0);
+  const [scoreArr, setScoreArr] = useState([]);
+  const focusRef = useRef(null);
+
+  const pushingArray = useCallback(() => {
+    setScoreArr([...scoreArr, 5 - fail]);
+  }, [scoreArr, fail]);
+
+  const calcScore = useCallback(() => {
+    const score = scoreArr.reduce((a, b) => (a + b)) / scoreArr.length;
+    setAroundScore(Math.round(score));
+  }, [scoreArr]);
+
+  const callPrompt = useCallback(() => {
+    setIndex(0);
+    setFail(0);
+    setScoreArr([]);
+    setIsStart(true);
+  }, []);
+
+  const onAnswer = useCallback(() => {
+    if (inputValue.toLowerCase() === values[index]) {
+      setRightTranslate(true);
+      pushingArray();
+      setIndex(index + 1);
+      setInputValue('');
+    } else {
+      setRightTranslate(false);
+      setFail(fail + 1);
+      setInputValue('');
+    }
+    focusRef.current.focus();
+  }, [inputValue, index, fail]);
+
+  useEffect(() => {
+    if (fail >= 3) {
+      setIndex(index + 1);
+      setFail(0);
+      pushingArray();
+    }
+  }, [index, fail]);
+
+  useEffect(() => {
+    if (index === keys.length) {
+      setIsStart(false);
+      calcScore();
+      onAnswer();
+      setStatus(`Тест закончен, ваша оценка ${aroundScore}`);
+    }
+  }, [index, calcScore, onAnswer]);
+
+  useEffect(() => {
+    if (isStart) {
+      setStatus(`Переведите слово ${keys[index]}`);
+      if (index >= 1 && index < keys.length) {
+        setStatus(`Переведите слово ${keys[index]}`);
+      }
+    }
+  }, [isStart, index]);
+
+  return (
+    <div style={style.container}>
+      <div style={style.startDiv}>
+        <p style={style.tittle}>
+          Тест на знание слов
+        </p>
+        <Button disabled={isStart} onPress={callPrompt} text="Старт" primary />
+      </div>
+      <div style={style.containerInput}>
+        <TextInput
+          ref={focusRef}
+          value={inputValue}
+          onChange={(e) => setInputValue(e.target.value)}
+        />
+        <Button disabled={!isStart} onPress={onAnswer} text="Ввод" outline />
+      </div>
+      <div style={style.status}>
+        <h3>{status}</h3>
+      </div>
+      {isStart && rightTranslate !== undefined && (
+      <div style={style.status}>
+        <h3 style={style.statusText}>{rightTranslate ? 'Good' : 'Bad'}</h3>
+        <h4 style={style.statusText}>{rightTranslate ? 'next word' : 'try again'}</h4>
+      </div>
+      )}
+    </div>
+  );
+});
+
+export default TranslateTest;
---
+++ src/components/Slider/TranslateTest/styles.js
@@ -0,0 +1,56 @@
+const styles = {
+  container: {
+    padding: 0,
+    boxSizing: 'border-box',
+    fontFamily: 'Comfortaa, cursive',
+    width: '70%',
+    margin: '0 auto',
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  startDiv: {
+    border: '5px solid teal',
+    borderRadius: 50,
+    margin: 30,
+    width: '50%',
+    justifyContent: 'center',
+    height: 100,
+    backgroundColor: 'rgba(0, 128, 128, 0.13)',
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+  },
+  tittle: {
+    width: '70%',
+    textAlign: 'center',
+    marginBottom: 15,
+  },
+  containerInput: {
+    width: '100%',
+    height: '100%',
+    display: 'flex',
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  status: {
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginTop: 25,
+    width: '50%',
+    height: 70,
+    textAlign: 'center',
+    backgroundColor: 'rgba(0, 128, 128, 0.266)',
+    borderRadius: 10,
+  },
+  statusText: {
+    margin: 5,
+    padding: 0,
+  },
+};
+
+export default styles;
